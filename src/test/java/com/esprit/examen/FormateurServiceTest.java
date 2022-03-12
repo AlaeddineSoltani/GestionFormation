9b4477b452cf1c25e0e7443543a2e8b4a2ebd44b
Ala Eddine SOLTANI <aesoltani@vermeg.com>
2022-03-12 11:32:50 +0100

    pipeline test

--- src/test/java/com/esprit/examen/FormateurServiceTest.java
+++ src/test/java/com/esprit/examen/FormateurServiceTest.java
@@ -9,7 +9,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.util.ArrayList;
 import java.util.List;
 
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.AfterEach;
@@ -18,108 +17,115 @@ import org.junit.jupiter.api.Test;
 import com.esprit.examen.services.FormateurService;
 import com.esprit.examen.entities.*;
 import com.esprit.examen.repositories.FormateurRepository;
+
 @SpringBootTest
-public class FormateurServiceTest { 
+public class FormateurServiceTest {
 	@Autowired
-	FormateurService formateurService ;
+	FormateurService formateurService;
 	@Autowired
 	FormateurRepository formateurRepository;
 	private static Contrat CONTRAT = Contrat.CIVP;
 	private static String EMAIL = "felBenFoulen@gmail.com";
-	private static String PASSWORD ="password" ;
+	private static String PASSWORD = "password";
 	private static String NOM = "flen";
 	private static String PRENOM = "ben foulen";
-	private static Poste POST=  Poste.INGENIEUR;
-	private Formateur testformateur = new Formateur( NOM, PRENOM, POST, CONTRAT, EMAIL, PASSWORD);
-    private Formateur testformateur2 = new Formateur( "f1", "P1", Poste.TECHINICIEN, Contrat.EXPERT, "f1@gamil.com", "passF1");
-    private Formateur testformateur3 = new Formateur( "f2", "P1", Poste.INGENIEUR, Contrat.FREELANCE, "f2@gmail.com", "PasF2");
-    private static final Logger logger = LogManager.getLogger(FormateurServiceTest.class);
+	private static Poste POST = Poste.INGENIEUR;
+	private Formateur testformateur = new Formateur(NOM, PRENOM, POST, CONTRAT, EMAIL, PASSWORD);
+	private Formateur testformateur2 = new Formateur("f1", "P1", Poste.TECHINICIEN, Contrat.EXPERT, "f1@gamil.com",
+			"passF1");
+	private Formateur testformateur3 = new Formateur("f2", "P1", Poste.INGENIEUR, Contrat.FREELANCE, "f2@gmail.com",
+			"PasF2");
+	private static final Logger logger = LogManager.getLogger(FormateurServiceTest.class);
 
 	@BeforeEach
 	void clearDataBase() {
-	   formateurRepository.deleteAll();
+		formateurRepository.deleteAll();
 	}
+
 	@Test
-	public void testAddFormateur(){
-	logger.debug("entring testAddFormateur methode");
-	long start = System.currentTimeMillis();
-	formateurService.addFormateur(testformateur);
-	assertTrue(formateurService.listFormateurs().contains(testformateur));
-		long elapsedTime = System.currentTimeMillis() - start ;
-	logger.info("Method execution time: " + elapsedTime + " milliseconds.");
-	logger.debug("quiting testAddFormateur methode");
-	formateurService.supprimerFormateur(testformateur.getId());
-	}
-	@Test
-	public void testDeleteFormateur(){
-	logger.debug("entring testDeleteFormateur methode");
-	long start = System.currentTimeMillis();
-	formateurService.addFormateur(testformateur);
+	public void testAddFormateur() {
+		logger.debug("entring testAddFormateur methode");
+		long start = System.currentTimeMillis();
+		formateurService.addFormateur(testformateur);
 		assertTrue(formateurService.listFormateurs().contains(testformateur));
-     formateurService.supprimerFormateur(testformateur.getId());
-     assertFalse(formateurService.listFormateurs().contains(testformateur));
-		long elapsedTime = System.currentTimeMillis() - start ;
-	logger.info("Method execution time: " + elapsedTime + " milliseconds.");
-	logger.debug("quiting testAddFormateur methode");	
+		long elapsedTime = System.currentTimeMillis() - start;
+		logger.info("Method execution time: " + elapsedTime + " milliseconds.");
+		logger.debug("quiting testAddFormateur methode");
+		formateurService.supprimerFormateur(testformateur.getId());
 	}
+
 	@Test
-	public void testmodifierFormateur(){
+	public void testDeleteFormateur() {
+		logger.debug("entring testDeleteFormateur methode");
+		long start = System.currentTimeMillis();
+		formateurService.addFormateur(testformateur);
+		assertTrue(formateurService.listFormateurs().contains(testformateur));
+		formateurService.supprimerFormateur(testformateur.getId());
+		assertFalse(formateurService.listFormateurs().contains(testformateur));
+		long elapsedTime = System.currentTimeMillis() - start;
+		logger.info("Method execution time: " + elapsedTime + " milliseconds.");
+		logger.debug("quiting testAddFormateur methode");
+	}
+
+	@Test
+	public void testmodifierFormateur() {
 		logger.debug("entring testmodifierFormateur methode");
 		long start = System.currentTimeMillis();
-        formateurService.addFormateur(testformateur);
+		formateurService.addFormateur(testformateur);
 		testformateur.setNom("new flen");
 		testformateur.setPassword("new password");
 		formateurService.modifierFormateur(testformateur);
 		Formateur formateurModifier = (Formateur) formateurService.listFormateurs().get(0);
 		assertThat("new flen").isEqualTo(formateurModifier.getNom());
 		assertThat("new password").isEqualTo(formateurModifier.getPassword());
-		long elapsedTime = System.currentTimeMillis() - start ;
+		long elapsedTime = System.currentTimeMillis() - start;
 		logger.info("Method execution time: " + elapsedTime + " milliseconds.");
 		logger.debug("quiting testmodifierFormateur methode");
 	}
+
 	@Test
-	public void testListFormateurs(){
+	public void testListFormateurs() {
 		logger.debug("entring testmodifierFormateur methode");
 		long start = System.currentTimeMillis();
-		List<Formateur> listFormateur = (List<Formateur>) new ArrayList<Formateur>(); 
+		List<Formateur> listFormateur = (List<Formateur>) new ArrayList<Formateur>();
 		listFormateur.add(testformateur);
 		listFormateur.add(testformateur2);
 		listFormateur.add(testformateur3);
 		ArrayList<Formateur> listFormateur2 = ((ArrayList<Formateur>) listFormateur);
-		for ( Formateur f : listFormateur2 ){
+		for (Formateur f : listFormateur2) {
 			formateurService.addFormateur(f);
 		}
-		List<Formateur> listFormateurSaved = (List<Formateur>) formateurService.listFormateurs(); 
+		List<Formateur> listFormateurSaved = (List<Formateur>) formateurService.listFormateurs();
 		assertTrue(listFormateurSaved.contains(testformateur));
 		assertTrue(listFormateurSaved.contains(testformateur2));
 		assertTrue(listFormateurSaved.contains(testformateur3));
-		long elapsedTime = System.currentTimeMillis() - start ;
+		long elapsedTime = System.currentTimeMillis() - start;
 		logger.info("Method execution time: " + elapsedTime + " milliseconds.");
 		logger.debug("quiting testmodifierFormateur methode");
 	}
+
 	@Test
-	public void testCountFormateur(){
+	public void testCountFormateur() {
 		logger.debug("entring testmodifierFormateur methode");
 		long start = System.currentTimeMillis();
-		List<Formateur> listFormateur = (List<Formateur>) new ArrayList<Formateur>(); 
-    	assertThat(0).isEqualTo(listFormateur.size());
+		List<Formateur> listFormateur = (List<Formateur>) new ArrayList<Formateur>();
+		assertThat(0).isEqualTo(listFormateur.size());
 		listFormateur.add(testformateur);
 		listFormateur.add(testformateur2);
 		listFormateur.add(testformateur3);
-		for ( Formateur f : (ArrayList<Formateur>) listFormateur ){
+		for (Formateur f : (ArrayList<Formateur>) listFormateur) {
 			formateurService.addFormateur(f);
 		}
-		List<Formateur> listFormateurSaved = (List<Formateur>) formateurService.listFormateurs(); 
-     	assertThat(3).isEqualTo(listFormateurSaved.size());
-		long elapsedTime = System.currentTimeMillis() - start ;
+		List<Formateur> listFormateurSaved = (List<Formateur>) formateurService.listFormateurs();
+		assertThat(3).isEqualTo(listFormateurSaved.size());
+		long elapsedTime = System.currentTimeMillis() - start;
 		logger.info("Method execution time: " + elapsedTime + " milliseconds.");
 		logger.debug("quiting testmodifierFormateur methode");
 	}
 
 	@AfterEach
 	void setUpDataBase() {
-	   formateurRepository.deleteAll();
-	}
-	 
+		formateurRepository.deleteAll();
 	}
 
+}
